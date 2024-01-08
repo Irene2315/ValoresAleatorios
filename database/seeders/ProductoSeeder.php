cf51e9603598e8f916b2d4ca77d4b4287c03674f
Irene <ireneherrero@outlook.com>
2024-01-08 12:39:50 +0100

    version_Aleatoria

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -13,28 +13,7 @@ class ProductoSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {/*
-        $productos = [
-            [
-                'nombre' => 'Chocolate',
-                'descripcion' => 'Alimento estimulante nutritivo',
-                'cantidad' => '1'
-            ],
-            [
-                'nombre' => 'Leche',
-                'descripcion' => 'Contiene lactosa',
-                'cantidad' => '10'
-            ],
-            [
-                'nombre' => 'Manzana',
-                'descripcion' => 'Fruta tipo acida',
-                'cantidad' => '15'
-            ]
-        ];
-
-        foreach ($productos as $producto) {
-            Producto::create($producto);
-        }*/
+    {
 
         Producto::factory(5)->create();
     }
